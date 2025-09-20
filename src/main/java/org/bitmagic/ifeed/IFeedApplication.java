95f155a8bb2a3716deb4b2f9d0fc268a89254de0
weekend-project <sunxzg@gmail.com>
2025-09-20 18:09:35 +0800

    init repo

---
+++ src/main/java/org/bitmagic/ifeed/IFeedApplication.java
@@ -0,0 +1,13 @@
+package org.bitmagic.ifeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IFeedApplication.class, args);
+	}
+
+}
